31344aca599b5fbd775e111ed243a7e7656e4f06
Jordan Mesches <zippedzipper5@gmail.com>
2019-04-24 22:26:31 -0400

    Update Basic-Login V.2
    Added Login, used hideEchoBack = true, so when typing password the characters are hiden and shown as '*'

--- Login.js
+++ Login.js
@@ -3,7 +3,8 @@ var standard_input = process.stdin;
 var newusername;
 var rl = require('readline-sync');
 var newpassword;
-var response = '';
+var username;
+var password;
 standard_input.setEncoding('utf-8');
 
 let con = mysql.createConnection({
@@ -21,14 +22,26 @@ con.connect(function(err){
 	standard_input.on('data', function(data){
 
 		if(data === 'register\n'){
-		newusername = rl.question("Enter New User:");
-		newpassword = rl.question("Enter New Password:");
+		newusername = rl.question("Enter New Username:");
+		newpassword = rl.question("Enter New Password:", {hideEchoBack: true});
 			
 
 			//enter data into database
 			var newaccount = "INSERT INTO users (username, password) VALUES ("+"'" + newusername + "'" + "," + "'" + newpassword + "'" + ")";
 			con.query(newaccount, function (err, result){
 				console.log("New Account Made!");
+			});
+		}
+		if(data === 'login\n'){
+			username = rl.question("Enter Username:");
+			password = rl.question("Enter Password:", {hideEchoBack: true});
+
+			//checks database for names
+			var login = "SELECT * FROM users WHERE username = " + "'" + username + "'" + "AND password = " + "'" + password + "'";
+			con.query(login, function (err, result){
+				if(err) throw err;
+				console.log(result);
+				console.log("login");
 			});
 		}
 	});
